a45cf327f19ac004ee9c6aa9d642b3941883e94c
Olga Vaneeva <146543384+vaneevaolya@users.noreply.github.com>
2025-04-20 00:42:24 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,14 +1,17 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World"); // This will show up in Render's logs
+
+// Optional route if you want the browser to see something too
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('App is running');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
-
 
 
 
